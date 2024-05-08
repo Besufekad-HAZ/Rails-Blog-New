97e4bc66d72c61c5afd8bb321a2693e01f467126
Besufekad-HAZ <besurye@gmail.com>
2024-05-09 00:20:20 +0300

    Add delete AKA Destoy routes in the route file

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   patch '/blog_posts/:id', to: 'blog_posts#update'
 
+  delete '/blog_posts/:id', to: 'blog_posts#destroy'
+
   get '/blog_posts/:id/edit', to: 'blog_posts#edit', as: :edit_blog_post
 
   post '/blog_posts', to: 'blog_posts#create', as: :blog_posts
