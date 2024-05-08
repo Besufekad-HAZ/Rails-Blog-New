439188e9546e60d4227dedc6ac502093611f8900
Besufekad-HAZ <besurye@gmail.com>
2024-05-08 15:53:43 +0300

    Install Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  get '/blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
 
-  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-
+  get '/blog_posts/:id', to: 'blog_posts#show', as: :blog_post
 
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  root 'blog_posts#index'
 end
