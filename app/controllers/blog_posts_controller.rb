31d917df96715ef4b41aed842cd7982b2a6dcba0
Besufekad-HAZ <besurye@gmail.com>
2024-05-08 14:39:59 +0300

    Add redirect in the show controller to handle invalid blogs

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,5 +5,7 @@ class BlogPostsController < ApplicationController
 
   def show
     @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
