0296b3a9a9f20531b76db8542811a5f4040c4ffc
Besufekad-HAZ <besurye@gmail.com>
2024-05-08 15:58:00 +0300

    Fix the NoMethodError in BlogPosts#new in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get '/blog_posts/:id', to: 'blog_posts#show', as: :blog_post
 
+  post '/blog_posts', to: 'blog_posts#create', as: :blog_posts
+
   # Defines the root path route ("/")
   root 'blog_posts#index'
 end
