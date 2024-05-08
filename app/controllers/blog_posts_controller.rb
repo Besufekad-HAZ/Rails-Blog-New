eac2508eb67679e8e99301fe274e2367038fea5b
Besufekad-HAZ <besurye@gmail.com>
2024-05-08 16:57:18 +0300

    Succesfully create a new blog and save it to the database perfectly

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -14,4 +14,20 @@ class BlogPostsController < ApplicationController
   def new
     @blog_post = BlogPost.new
   end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
 end
