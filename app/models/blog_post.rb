29e5a033807eed5b3916e8dc793fa273c97fbfd9
Besufekad-HAZ <besurye@gmail.com>
2024-05-08 16:59:24 +0300

    Add validation to blogpost model

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class BlogPost < ApplicationRecord
+
+  validates :title, presence: true
+  validates :body, presence: true
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 end
