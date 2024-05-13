a1cd390fdbc3692fa235dc715f1e7776d518a163
Besufekad-HAZ <besurye@gmail.com>
2024-05-13 13:07:11 +0300

    Add Rubocop fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ApplicationRecord
+class User < ApplicationRecord # rubocop:disable Style/Documentation
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :registerable, and :omniauthable
   devise :database_authenticatable,
