c4cec525a7ab70809855b237d9257859b2f8b4c0
Besufekad-HAZ <besurye@gmail.com>
2024-05-22 13:59:23 +0300

    Write 4 working tests to check drafts and published ones

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -3,8 +3,19 @@
 require 'test_helper'
 
 class BlogPostTest < ActiveSupport::TestCase
-   test "draft? returns true for draft blog post" do
-    binding.irb
-    BlogPost.new(published_at: nil).draft?
-   end
+  test 'draft? returns true for draft blog post' do
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test 'draft? returns false for published blog post' do
+    refute BlogPost.new(published_at: 1.day.ago).draft?
+  end
+
+  test 'draft? returns false for schduled blog post' do
+    refute BlogPost.new(published_at: 1.day.from_now).draft?
+  end
+
+  test 'published? returns false for draft blog post' do
+    refute BlogPost.new(published_at: nil).published?
+  end
 end
