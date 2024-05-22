b27898423fac43798a32052020a3087bf0610a0a
Besufekad-HAZ <besurye@gmail.com>
2024-05-22 14:22:48 +0300

    Used Helper Function and implement all the needed tests

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -5,41 +5,56 @@ require 'test_helper'
 class BlogPostTest < ActiveSupport::TestCase
   # Test for draft? method
   test 'draft? returns true for draft blog post' do
-    assert BlogPost.new(published_at: nil).draft?
+    assert draft_blog_post.draft?
   end
 
   test 'draft? returns false for published blog post' do
-    refute BlogPost.new(published_at: 1.day.ago).draft?
+    refute published_blog_post.draft?
   end
 
   test 'draft? returns false for schduled blog post' do
-    refute BlogPost.new(published_at: 1.day.from_now).draft?
+    refute scheduled_blog_post.draft?
   end
 
   # Test for published? method
   test 'published? returns true for published blog post' do
-    assert BlogPost.new(published_at: 1.month.ago).published?
+    assert published_blog_post.published?
   end
 
   test 'published? returns false for draft blog post' do
-    refute BlogPost.new(published_at: nil).published?
+    refute draft_blog_post.published?
   end
 
   test 'published? returns false for scheduled blog post' do
-    refute BlogPost.new(published_at: 1.day.from_now).published?
+    refute scheduled_blog_post.published?
   end
 
   # Test for scheduled? method
   test 'scheduled? returns true for scheduled blog post' do
-    assert BlogPost.new(published_at: 1.month.from_now).scheduled?
+    assert scheduled_blog_post.scheduled?
   end
 
   test 'scheduled? returns false for draft blog post' do
-    refute BlogPost.new(published_at: nil).scheduled?
+    refute draft_blog_post.scheduled?
   end
 
   test 'published? returns false for published blog post' do
-    refute BlogPost.new(published_at: 1.day.ago).scheduled?
+    refute published_blog_post.scheduled?
+  end
+
+  # Helper function for draft_blog_post
+  def draft_blog_post
+    BlogPost.new(published_at: nil)
+  end
+
+  # Helper function for published_blog_post
+  def published_blog_post
+    BlogPost.new(published_at: 1.month.ago)
+  end
+
+  # Helper function for scheduled_blog_post
+  def scheduled_blog_post
+    BlogPost.new(published_at: 1.month.from_now)
   end
 
   # Test for to_param method
