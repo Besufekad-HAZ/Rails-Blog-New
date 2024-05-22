8d6950209df20aeab71a914c2aaff836e4818c3a
Besufekad-HAZ <besurye@gmail.com>
2024-05-22 11:04:10 +0300

    Add Scope in the blog_post model to return not published yet, published or to be published in the futur

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -4,6 +4,10 @@ class BlogPost < ApplicationRecord # rubocop:disable Style/Documentation
   validates :title, presence: true
   validates :body, presence: true
 
+  scope :draft, -> { where(published_at: nil) }
+  scope :published, -> { where('published_at <= ?', Time.current) }
+  scope :scheduled, -> { where('published_at > ?', Time.current) }
+
   def to_param
     "#{id}-#{title.parameterize}"
   end
