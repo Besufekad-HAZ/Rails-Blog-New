0d44b9b0ae599468a2f49c01bfbcdde51435ea00
Besufekad-HAZ <besurye@gmail.com>
2024-06-10 23:16:38 +0300

    Fix the delete url implementation perfectly

--- app/controllers/blog_posts/cover_images_controller.rb
+++ app/controllers/blog_posts/cover_images_controller.rb
@@ -1,10 +1,16 @@
 class BlogPosts::CoverImagesController < ApplicationController
+  include ActionView::RecordIdentifier
+
   before_action :authenticate_user!
   before_action :set_blog_post
 
   def destroy
     @blog_post.cover_image.purge_later
-    redirect_to edit_blog_post_path(@blog_post)
+
+    respond_to do |format|
+      format.html { redirect_to edit_blog_post_path(@blog_post) }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@blog_post, :cover_image)) }
+    end
   end
 
   private
