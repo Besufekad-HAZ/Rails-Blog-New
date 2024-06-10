399d745f4575dbc390666781da2a76722a1c700b
Besufekad-HAZ <besurye@gmail.com>
2024-06-10 19:41:12 +0300

    Update the layout and styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,16 +1,20 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        selected: '#092215',
+        unselected: '#98FB98',
       },
     },
   },
@@ -18,5 +22,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
-}
+  ],
+};
