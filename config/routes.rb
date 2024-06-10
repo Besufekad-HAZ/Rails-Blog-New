dedfb88defd2239aed257c8327d95ab74b95b966
Besufekad-HAZ <besurye@gmail.com>
2024-06-10 22:54:27 +0300

    First debug attempt on deleting the url image

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   resources :blog_posts do
-    resources :cover_image, only: [:destroy], module: :blog_posts
+    resource :cover_image, only: [:destroy], module: :blog_posts
   end
 
   # Defines the root path route ("/")
